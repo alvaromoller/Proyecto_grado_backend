e31dfadce622438b27b2e561f855ea983d0c916e
alvaromoller <alvaromollerlima@gmail.com>
2021-10-26 16:38:43 -0400

    agregando getter and setter de transaction

--- src/main/java/webcrawler/prueba/model/Shop.java
+++ src/main/java/webcrawler/prueba/model/Shop.java
@@ -1,5 +1,7 @@
 package webcrawler.prueba.model;
 
+import java.util.Date;
+
 public class Shop {
     private Integer shopId;
     private String name;
@@ -47,5 +49,64 @@ public class Shop {
 
     public void setStatus(Integer status) {
         this.status = status;
+    }
+
+    //transaction
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
+
+    //fin transaction
+
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "shopId=" + shopId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", status=" + status +
+                ", txId=" + transaction.getTxId() +
+                ", txHost='" + transaction.getTxHost() + '\'' +
+                ", txUserId=" + transaction.getTxUserId() +
+                ", txDate=" + transaction.getTxDate() +
+                '}';
     }
 }
