7e899c798f79e2a222551c2ad02306b84da721c0
alvaromoller <alvaromollerlima@gmail.com>
2021-10-26 13:43:04 -0400

    agregando transaction en la clase Product

--- src/main/java/webcrawler/prueba/model/Product.java
+++ src/main/java/webcrawler/prueba/model/Product.java
@@ -1,13 +1,15 @@
 package webcrawler.prueba.model;
 
+import java.util.Date;
+
 public class    Product {
     private Integer productId;
     private String name;
     private String description;
     private String img;
     private Integer status;
-    //falta agregar un transaction
-
+    //transaction
+    private Transaction transaction;
 
     public Integer getProductId() {
         return productId;
@@ -47,5 +49,65 @@ public class    Product {
 
     public void setStatus(Integer status) {
         this.status = status;
+    }
+
+    //transaction
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
+    
+    //fin transaction
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", img='" + img + '\'' +
+                ", status=" + status +
+                ", txId=" + transaction.getTxId() +
+                ", txHost='" + transaction.getTxHost() + '\'' +
+                ", txUserId=" + transaction.getTxUserId() +
+                ", txDate=" + transaction.getTxDate() +
+                '}';
     }
 }
