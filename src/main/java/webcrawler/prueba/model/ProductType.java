60af1b7ee922a43e03f8ec1a4ad15bfd435ba57d
alvaromoller <alvaromollerlima@gmail.com>
2021-10-26 16:43:13 -0400

    creando clase ProductType

---
+++ src/main/java/webcrawler/prueba/model/ProductType.java
@@ -0,0 +1,32 @@
+package webcrawler.prueba.model;
+
+public class ProductType {
+    private Integer productTypeId;
+    private String name;
+    private Integer status;
+    private Transaction transaction;
+
+    public Integer getProductTypeId() {
+        return productTypeId;
+    }
+
+    public void setProductTypeId(Integer productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
