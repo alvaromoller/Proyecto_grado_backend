cb286b33a8c113d9f3cc09bfa4cca679741c85f5
alvaromoller <alvaromollerlima@gmail.com>
2021-10-27 17:55:32 -0400

    creando clase ProductoDto Para la comunicacion con BL

---
+++ src/main/java/webcrawler/prueba/dto/ProductDto.java
@@ -0,0 +1,92 @@
+package webcrawler.prueba.dto;
+
+public class ProductDto {
+    //clase ProductoDto
+    //sin status ni transaction
+    private Integer productId;
+    private String name;
+    private String description;
+    private String img;
+    private Integer brandId;
+    private Integer shopId;
+    private Integer productTypeId;
+
+    /** img
+    public java.lang.String getImg() {
+        return img;
+    }
+
+    public void setImg(java.lang.String img) {
+        this.img = img;
+    }
+     */
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
+
+    public Integer getProductTypeId() {
+        return productTypeId;
+    }
+
+    public void setProductTypeId(Integer productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDto{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", img='" + img + '\'' +
+                ", brandId=" + brandId +
+                ", shopId=" + shopId +
+                ", productTypeId=" + productTypeId +
+                '}';
+    }
+}
