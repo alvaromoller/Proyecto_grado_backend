cbd8e74c8d8c6d479a8b82ebeff9bbe6b205a382
alvaromoller <alvaromollerlima@gmail.com>
2021-10-30 19:12:30 -0400

    creando clase ShopBl

---
+++ src/main/java/webcrawler/prueba/bl/ShopBl.java
@@ -0,0 +1,39 @@
+package webcrawler.prueba.bl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webcrawler.prueba.dao.ShopDao;
+import webcrawler.prueba.dto.ShopDto;
+import webcrawler.prueba.model.Shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ShopBl {
+    private ShopDao shopDao;
+
+    @Autowired
+    public ShopBl (ShopDao shopDao){
+        this.shopDao = shopDao;
+    }
+
+    //listado de Tiendas
+    public List<ShopDto> selectShops(){
+        List<Shop> shops = shopDao.getShops();
+        List<ShopDto> shopDtos = new ArrayList<ShopDto>();
+
+        for(int i=0; i < shops.size(); i++){
+            Shop shop = shops.get(i);
+            ShopDto shopDto = new ShopDto();
+            shopDto.setShopId(shop.getShopId());
+            shopDto.setName(shop.getName());
+            shopDto.setDescription(shop.getDescription());
+            shopDto.setLocation(shop.getLocation());
+
+            shopDtos.add(i, shopDto);
+        }
+        return shopDtos;
+    }
+
+}
