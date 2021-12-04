faee61900cb8da0a2d2eb98f7a8ee58cf338c4f0
alvaromoller <alvaromollerlima@gmail.com>
2021-12-04 12:30:50 -0400

    agregando clase modelo ProductDetail

---
+++ src/main/java/webcrawler/prueba/model/ProductDetail.java
@@ -0,0 +1,118 @@
+package webcrawler.prueba.model;
+
+import java.util.Date;
+
+public class ProductDetail {
+    private Integer productDetailId;
+    private Number price;
+    private Integer quantity;
+    private Integer productId;
+    private Integer status;
+
+
+    private Transaction transaction;
+
+    //constructor
+    public ProductDetail(){
+        this.transaction = new Transaction();
+    }
+
+    public Integer getProductDetailId() {
+        return productDetailId;
+    }
+
+    public void setProductDetailId(Integer productDetailId) {
+        this.productDetailId = productDetailId;
+    }
+
+    public Number getPrice() {
+        return price;
+    }
+
+    public void setPrice(Number price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    //transaction
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
+
+    //fin transaction
+
+
+    @Override
+    public String toString() {
+        return "ProductDetail{" +
+                "productDetailId=" + productDetailId +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", productId=" + productId +
+                ", status=" + status +
+                ", txId=" + transaction.getTxId() +
+                ", txHost='" + transaction.getTxHost() + '\'' +
+                ", txUserId=" + transaction.getTxUserId() +
+                ", txDate=" + transaction.getTxDate() +
+                '}';
+    }
+}
