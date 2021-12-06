46ca541423a9c66c39c6d3dd8c93b1fb6989f6fe
alvaromoller <alvaromollerlima@gmail.com>
2021-12-05 22:09:15 -0400

    extrayendo y guardando en BD precio de producto 1 de pagina 2

--- src/main/java/webcrawler/prueba/api/ProductDetailApi.java
+++ src/main/java/webcrawler/prueba/api/ProductDetailApi.java
@@ -9,8 +9,10 @@ import webcrawler.prueba.dto.ProductDetailDto;
 import webcrawler.prueba.model.Transaction;
 import webcrawler.prueba.util.TransactionUtil;
 import webcrawler.prueba.webCrawler.ComputerPageOne;
+import webcrawler.prueba.webCrawler.ComputerPageTwo;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -22,12 +24,14 @@ public class ProductDetailApi {
     private ProductDetailBl productDetailBl;
     private TransactionBl transactionBl;
     //private ComputerPageOne computerPageOne;
+    private ComputerPageTwo computerPageTwo;
+
 
     @Autowired
-    public ProductDetailApi (ProductDetailBl productDetailBl, TransactionBl transactionBl){
+    public ProductDetailApi (ProductDetailBl productDetailBl, ComputerPageTwo computerPageTwo, TransactionBl transactionBl){
         this.productDetailBl = productDetailBl;
+        this.computerPageTwo = computerPageTwo;
          this.transactionBl = transactionBl;
-       // this.computerPageOne = computerPageOne;
     }
 
 
@@ -52,6 +56,18 @@ public class ProductDetailApi {
         transactionBl.createTransaction(transaction);
         ProductDetailDto productDtoResponse = productDetailBl.createDetail(productDetailDto, transaction);
         return productDtoResponse;
+    }
+//TIENDA 1 NO TENIA SUS PRECIOS
+
+//TIENDA 2 DISMAC,
+    //producto 1
+    @RequestMapping(path ="/crawler4", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void crawler4(@RequestBody ProductDetailDto productDetailDto, HttpServletRequest request)throws IOException {
+        Transaction transaction = TransactionUtil.createTransaction(request);
+        transactionBl.createTransaction(transaction);
+        //dirección marca 1
+        String url="https://www.dismac.com.bo/o85pd.html";  //Pc1
+        computerPageTwo.extractDetail(url, productDetailDto, transaction);
     }
 
 
