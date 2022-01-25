a9bb7e6b67a7dc2449cf67ad75ae7c1c88ee9bc4
alvaromoller <alvaromollerlima@gmail.com>
2022-01-25 16:52:24 -0400

    creando clase

--- src/main/java/webcrawler/prueba/webSocket/controller/GreetingController.java
+++ src/main/java/webcrawler/prueba/webSocket/controller/GreetingController.java
@@ -1,4 +1,23 @@
 package webcrawler.prueba.webSocket.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+import webcrawler.prueba.webSocket.model.Greeting;
+import webcrawler.prueba.webSocket.model.HelloMessage;
+
+@Controller
 public class GreetingController {
+
+    //La  anotación @MessageMapping  garantiza que si se envía un mensaje al destino "/hello",   se llama al método greeting()
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception{
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+
 }
